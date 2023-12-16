ed5f6f1008addd8c77d3e95a7c68ea65179a10d4
Manfredi Pomar <152746148+mpomar@users.noreply.github.com>
2023-12-16 23:54:39 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+strava_id = "" # This is Strava's Client ID
+strava_secret = "" # This is Strava's Client Secret
+api_id = "" # This is Telegram API ID
+api_hash = "" # This is Telegram API hash
+bot_token = "" # This is the bot token from Botfather
